18f44c49470bc96b979c329aca5900c4efe3e76a
shobhitsundriyal <shobhitsundriyal70@gmail.com>
2024-10-23 12:34:43 +0530

    2.3 returning data from threads

--- hello_thread/src/main.rs
+++ hello_thread/src/main.rs
@@ -4,21 +4,26 @@ fn hello_thread(n: i32) {
     println!("Hello, from thread {}!", n);
 }
 
+fn double_number_n_times(number: u32, times: u32) -> u32  {
+    let mut result = number;
+    for _ in 0..times {
+        result *= 2;
+    }
+    return result;
+}
+
 fn main() {
     println!("Hello, from main thread!");
-
-    // let thread_handle = std::thread::spawn(hello_thread);
-    // thread_handle.join().unwrap(); // wait for thread to finish, sometimes it may happen threadd is not finished yet, so we need to wait for it to finish, if we comment this line, then the program might exit before the thread finishes
-
-    // // generaly its good idea to wait for thread to finish, so that we can get the result of the thread, if we don't wait for thread to finish, then we might not get the result of the thread
 
     let mut thread_handles = Vec::new();
 
     for i in 0..5 {
-        let thread_handle = std::thread::spawn(move || hello_thread(i));
+        let thread_handle = std::thread::spawn(move || double_number_n_times(i, 5));
         thread_handles.push(thread_handle);
     }
 
-    thread_handles.into_iter().for_each(|handle| handle.join().unwrap()); // wait for all threads to finish, not nesseary it would be in order, it can be in any order, as thread creation is handled by OS
+    thread_handles.into_iter().for_each(|handle| 
+        println!("{}", handle.join().unwrap())
+    );
 
 }
