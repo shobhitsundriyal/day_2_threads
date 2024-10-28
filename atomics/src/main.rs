bae8b0ea6825d953dee50c30796d8a44225a85e7
shobhitsundriyal <shobhitsundriyal70@gmail.com>
2024-10-28 07:26:42 +0530

    2.8 safe increment with atomics

--- atomics/src/main.rs
+++ atomics/src/main.rs
@@ -1,4 +1,6 @@
-static mut COUNTER: u32 = 0; // shared mutable state
+use std::sync::atomic::AtomicI32;
+
+static COUNTER: AtomicI32 = AtomicI32::new(0); // I am thinking it like js object, its not mutable but its value is mutable, also simple add operations are not allowed on atomic types, there are methods for that
 
 fn main() {
     let mut thread_handles = Vec::new();
@@ -6,9 +8,11 @@ fn main() {
     for _ in 0..1000 {
         let handle = std::thread::spawn(|| {
             for _ in 0..1_000 {
-                unsafe {
-                    COUNTER += 1;
-                }
+                COUNTER.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+                // ordering is relaxed because we dont care about the order of the operations
+                // there are other orderings as well
+                // Relaxed: No guarantees are made about the order of operations or memory visibility.
+                // sometimes we may want some ordering guarantees, but here it does not matter if thread1 increments before thread2 or vice versa as long as the they both do it synchronously
             }
         });
         thread_handles.push(handle);
@@ -16,7 +20,10 @@ fn main() {
 
     thread_handles.into_iter().for_each(|h| h. join().unwrap());
 
-    println!("Result: {}", unsafe { COUNTER });
+    println!("Result: {}",  COUNTER.load(std::sync::atomic::Ordering::Relaxed));
+    // now we are using load method to read the value of the counter, it would give us the correct counter val
+    // Result: 1_000_000
+
     // 1000 threads * 1000 increments = 1_000_000 this should be the answer
     /*
      result is alweys less than 1_000_000, also it is not deterministic
@@ -42,6 +49,5 @@ fn main() {
     /*
      we can use atomic types to solve this problem
      atomic types are types that can be safely shared between threads
-     a thread wont read a value that is being written by another thread
     */
 }
