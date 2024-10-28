6a08613d5d4953ef2571839f3731164805a6f8dc
shobhitsundriyal <shobhitsundriyal70@gmail.com>
2024-10-28 10:43:28 +0530

    2.11 poisoning

--- deadlock/src/main.rs
+++ deadlock/src/main.rs
@@ -1,16 +1,26 @@
 use std::sync::Mutex;
 
 static MY_SHARED_NUMBER: Mutex<i32> = Mutex::new(5);
-fn main() {
-     
-    let lock1 = MY_SHARED_NUMBER.lock().unwrap();
 
-    std::mem::drop(lock1); // manually release the lock1 
-
-    if let Ok(lock2) = MY_SHARED_NUMBER.try_lock() {
-        println!("Lock 2 is acquired");
-    } else {
-        println!("Lock 2 is not acquired");
-    }
-    // we can try to get lock2 here if lock1 is released, else lock2 will not be acquired
+fn poisioner() {
+    let mut num = MY_SHARED_NUMBER.lock().unwrap();
+    *num += 1;
+    panic!("poisioner panicked");
+    // thread would lock the mutex and panic, not releasing the lock
 }
+fn main() {
+    let hanlde = std::thread::spawn(poisioner); // this thread would panic without releasing the lock
+    let result = hanlde.join();
+    println!("{:?}", result);
+
+    // try to lock the mutex from main thread
+    let lock = MY_SHARED_NUMBER.lock();
+    println!("lock from main thread{:?}", lock);
+
+    // we can get the data from the mutex if we really need it
+    let recoverd_data = lock.unwrap_or_else(|poisoned| {
+        return poisoned.into_inner(); // this would return the data from the mutex even if it is poisoned(lock not released)
+    });
+    println!("recoverd data from mutex: {:?}", recoverd_data);
+}
+// rust calls this "Poisoning" the mutex
