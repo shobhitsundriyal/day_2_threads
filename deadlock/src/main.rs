37ab902abbdd267b5c15c7111715bd647aba68c1
shobhitsundriyal <shobhitsundriyal70@gmail.com>
2024-10-28 10:31:33 +0530

    2.11 try_lock

--- deadlock/src/main.rs
+++ deadlock/src/main.rs
@@ -1,8 +1,16 @@
 use std::sync::Mutex;
+
+static MY_SHARED_NUMBER: Mutex<i32> = Mutex::new(5);
 fn main() {
-    let shared_resource = Mutex::new(0);
-    {
-        let lock1 = shared_resource.lock().unwrap();
+     
+    let lock1 = MY_SHARED_NUMBER.lock().unwrap();
+
+    std::mem::drop(lock1); // manually release the lock1 
+
+    if let Ok(lock2) = MY_SHARED_NUMBER.try_lock() {
+        println!("Lock 2 is acquired");
+    } else {
+        println!("Lock 2 is not acquired");
     }
-    let lock2 = shared_resource.lock().unwrap();
+    // we can try to get lock2 here if lock1 is released, else lock2 will not be acquired
 }
