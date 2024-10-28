b9f0a2cd455b940e946dd65bbae109a47ddf156c
shobhitsundriyal <shobhitsundriyal70@gmail.com>
2024-10-28 11:20:06 +0530

    2.13 skipped parking thread

--- dashing/src/main.rs
+++ dashing/src/main.rs
@@ -3,3 +3,4 @@ fn main() {
 }
 // https://github.com/thebracket/ArdanUltimateRust-5Days/blob/main/02-SystemThreads/LockFree.md
 // lock free implementation, kuch zada smajh nahi aya
+// skipping pariking threads
