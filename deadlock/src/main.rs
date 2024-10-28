b1b99cbdef428f1027a7c8befa282b8712f314fb
shobhitsundriyal <shobhitsundriyal70@gmail.com>
2024-10-28 10:19:17 +0530

    2.11 simple way is to use scopes

--- deadlock/src/main.rs
+++ deadlock/src/main.rs
@@ -1,9 +1,8 @@
 use std::sync::Mutex;
 fn main() {
     let shared_resource = Mutex::new(0);
-
-    let lock1 = shared_resource.lock().unwrap();
+    {
+        let lock1 = shared_resource.lock().unwrap();
+    }
     let lock2 = shared_resource.lock().unwrap();
-    // This will cause a deadlock, as lock1 is already locked and the thread will wait for lock1 to be released
-    // while holding lock2. Progam will be stuck here.
 }
