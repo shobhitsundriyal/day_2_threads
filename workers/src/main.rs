f0d5fa597ce257d0d1e7652d7c1f9a76a6cf0dfa
shobhitsundriyal <shobhitsundriyal70@gmail.com>
2024-10-29 07:47:50 +0530

    2.15 added enums and modified to make more readable

--- workers/src/main.rs
+++ workers/src/main.rs
@@ -2,16 +2,24 @@ use std::sync::mpsc;
 
 type Job = Box<dyn FnOnce() + Send + 'static>; // kinda pointer to a function
 
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
+
 fn hi_there() {
     println!("Hi there!");
 }
 
 fn main() {
-    let (channel_sender, channel_receiver) = mpsc::channel::<Job>();
+    let (channel_sender, channel_receiver) = mpsc::channel::<Message>();
 
     let thread_handle = std::thread::spawn(move || {
-        while let Ok(job_fn) = channel_receiver.recv() {
-            job_fn(); // excute the job(function)
+        while let Ok(channel_message) = channel_receiver.recv() {
+            match channel_message {
+                Message::NewJob(job_fn) => job_fn(), // excute the job(function)
+                Message::Terminate => break
+            }
         }
     });
 
@@ -22,11 +30,14 @@ fn main() {
         }
     };
     
-    channel_sender.send(Box::new(hi_there)).unwrap();
-    channel_sender.send(Box::new(job1)).unwrap();
-    channel_sender.send(Box::new(job2)).unwrap();
+    channel_sender.send(Message::NewJob(Box::new(hi_there))).unwrap();
+    channel_sender.send(Message::NewJob(Box::new(job1))).unwrap();
+    channel_sender.send(Message::NewJob(Box::new(job2))).unwrap();
     // box is a smart pointer that points to heap memory, which points to function in this case
     // wrapping the function in a box so we get pointer to the function, that can be executed with () inside the thread
+
+    // send terminate message to the thread
+    channel_sender.send(Message::Terminate).unwrap();
 
     thread_handle.join().unwrap();
 }
